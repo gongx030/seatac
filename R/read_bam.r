bd776624a4a9f39cf23922d0ce36db54bee2140c
Wuming Gong <gongx030@umn.edu>
2020-01-22 11:35:27 -0600

    change read_bam.r to S4

--- R/read_bam.r
+++ R/read_bam.r
@@ -1,52 +1,97 @@
+setGeneric('read_bam', function(filenames, peaks, genome, ...) standardGeneric('read_bam'))
+
 #' read_bam
 #'
 #' Read the aligned paired-end reads over the given peaks. 
+#' @param filenames BAM file names 
+#' @param peaks a GRange object that define a set of genomic regions.
+#' @param genome a BSgenome for genome information
 #'  
-read_bam <- function(
-	filenames, 
-	peaks, 
-	genome
-){
+setMethod(
+	'read_bam',
+	signature(
+		filenames = 'character',
+		peaks = 'GRanges',
+		genome = 'BSgenome'
+	), 
+	function(filenames, peaks, genome, ...){
 
-	validate_bam(filenames)
+		validate_bam(filenames)
 
-  num_samples <- length(filenames)
+ 		num_samples <- length(filenames)
 
-  # genomic ranges covered by the BAM files
-  gr <- Reduce('intersect', lapply(filenames, function(f){
-    x <- idxstatsBam(f)
-    GRanges(seqnames = x[, 'seqnames'], range = IRanges(1, x[, 'seqlength']))
-  }))
-  seqlengths(seqinfo(gr)) <- width(gr)
-  genome(seqinfo(gr)) <- providerVersion(genome)
+	  # genomic ranges covered by the BAM files
+ 	 	gr <- Reduce('intersect', lapply(filenames, function(f){
+	    x <- idxstatsBam(f)
+			GRanges(seqnames = x[, 'seqnames'], range = IRanges(1, x[, 'seqlength']))
+ 	  }))
+		seqlengths(seqinfo(gr)) <- width(gr)
+	  genome(seqinfo(gr)) <- providerVersion(genome)
 
-  flag <- scanBamFlag(
-    isSecondaryAlignment = FALSE,
-    isUnmappedQuery = FALSE,
-    isNotPassingQualityControls = FALSE,
-    isProperPair = TRUE
-  )
+	  flag <- scanBamFlag(
+ 	  	isSecondaryAlignment = FALSE,
+	    isUnmappedQuery = FALSE,
+ 	  	isNotPassingQualityControls = FALSE,
+	    isProperPair = TRUE
+	  )
 
-	if (!missing(peaks)){
 		# set the seqlevels and seqlengths from BAM files to "which"
  		seqlevels(peaks, pruning.mode = 'coarse') <- seqlevels(gr)
 	  seqlevels(gr, pruning.mode = 'coarse') <- seqlevels(peaks)
 	  seqlengths(seqinfo(peaks)) <-  seqlengths(seqinfo(gr))
 	  genome(seqinfo(peaks)) <-  genome(seqinfo(gr))
-  	param <- ScanBamParam(which = reduce(peaks), flag = flag, what = 'isize')
-	}else{
-  	param <- ScanBamParam(flag = flag, what = 'isize')
+ 		param <- ScanBamParam(which = reduce(peaks), flag = flag, what = 'isize')
+
+		# Read the PE reads 
+	  x <- Reduce('c', lapply(seq_len(length(filenames)), function(i){
+ 	  	flog.info(sprintf('reading %s', filenames[i]))
+	    ga <- readGAlignments(filenames[i], param = param)
+			ga
+		}))
+		metadata(x)$num_samples <- num_samples
+		x
 	}
+) # read_bam
 
-	# Read the PE reads 
-  x <- Reduce('c', lapply(1:num_samples, function(i){
-    flog.info(sprintf('reading %s', filenames[i]))
-    ga <- readGAlignments(filenames[i], param = param)
-		ga
-	}))
-	metadata(x)$num_samples <- num_samples
-	x
 
-} # read_bam
+setMethod(
+	'read_bam',
+	signature(
+		filenames = 'character',
+		peaks = 'missing',
+		genome = 'BSgenome'
+	), 
+	function(filenames, peaks, genome, ...){
 
+		validate_bam(filenames)
+
+ 		num_samples <- length(filenames)
+
+	  # genomic ranges covered by the BAM files
+ 	 	gr <- Reduce('intersect', lapply(filenames, function(f){
+	    x <- idxstatsBam(f)
+			GRanges(seqnames = x[, 'seqnames'], range = IRanges(1, x[, 'seqlength']))
+ 	  }))
+		seqlengths(seqinfo(gr)) <- width(gr)
+	  genome(seqinfo(gr)) <- providerVersion(genome)
+
+	  flag <- scanBamFlag(
+ 	  	isSecondaryAlignment = FALSE,
+	    isUnmappedQuery = FALSE,
+ 	  	isNotPassingQualityControls = FALSE,
+	    isProperPair = TRUE
+	  )
+
+ 		param <- ScanBamParam(flag = flag, what = 'isize')
+
+		# Read the PE reads 
+	  x <- Reduce('c', lapply(seq_len(length(filenames)), function(i){
+ 	  	flog.info(sprintf('reading %s', filenames[i]))
+	    ga <- readGAlignments(filenames[i], param = param)
+			ga
+		}))
+		metadata(x)$num_samples <- num_samples
+		x
+	}
+) # read_bam
 
--- R/read_fragment_size_profile.r
+++ R/read_fragment_size_profile.r
@@ -1,14 +1,14 @@
 #' read_fragment_size_profile
 #'
 #' @param x a GAlignments object.
-#' @param windows a GRange object that define a set of genomic regions.
+#' @param peaks a GRange object that define a set of genomic regions.
 #' @param fragment_size_range fragment_size_range
 #'
 #' @export
 #'
 read_fragment_size_profile <- function(
 	x, 
-	windows, 
+	peaks, 
 	max_isize = 1500
 ){
 
@@ -24,11 +24,11 @@ read_fragment_size_profile <- function(
 	)
 	x <- x[x$isize <= max_isize]
 
-	WR <- as.matrix(findOverlaps(windows, x))	# window ~ reads
-	WR <- sparseMatrix(i = WR[, 1], j = WR[, 2], dims = c(length(windows), length(x))) %>% as('dgCMatrix')	# window ~ reads
+	WR <- as.matrix(findOverlaps(peaks, x))	# window ~ reads
+	WR <- sparseMatrix(i = WR[, 1], j = WR[, 2], dims = c(length(peaks), length(x))) %>% as('dgCMatrix')	# window ~ reads
 	RF <- sparseMatrix(i = 1:length(x), j = x$isize, dims = c(length(x), max_isize)) %>% as('dgCMatrix')	# reads ~ fragment size
-	mcols(windows)$fragment_size_profile <- WR %*% RF # window ~ fragment size
-	windows
+	mcols(peaks)$fragment_size_profile <- WR %*% RF # window ~ fragment size
+	peaks
 
 } # read_fragment_size_profile
 
