04e363fbdaefc3a223e73ded680961850a009be0
Wuming Gong <gongx030@umn.edu>
2020-08-17 15:27:25 -0500

    update

--- R/AllGeneric.r
+++ R/AllGeneric.r
@@ -1,16 +1,16 @@
-setGeneric('array_reshape', function(x, dims, ...) standardGeneric('array_reshape'))
+#setGeneric('array_reshape', function(x, dims, ...) standardGeneric('array_reshape'))
 
-setGeneric('get_motif_vplot', function(x, pwms, ...) standardGeneric('get_motif_vplot'))
+#setGeneric('get_motif_vplot', function(x, pwms, ...) standardGeneric('get_motif_vplot'))
 
-setGeneric('as_sparse_array', function(x, ...) standardGeneric('as_sparse_array'))
+#setGeneric('as_sparse_array', function(x, ...) standardGeneric('as_sparse_array'))
 
 setGeneric('read_bam', function(filename, peaks, genome, ...) standardGeneric('read_bam'))
 
 setGeneric('get_counts', function(x, filenames, genome, ...) standardGeneric('get_counts'))
 
-setGeneric('array_permute', function(x, perm, ...) standardGeneric('array_permute'))
+#setGeneric('array_permute', function(x, perm, ...) standardGeneric('array_permute'))
 
-setGeneric('compute_deviations', function(x, ...) standardGeneric('compute_deviations'))
+#setGeneric('compute_deviations', function(x, ...) standardGeneric('compute_deviations'))
 
 setGeneric('read_vplot', function(x, filenames, genome, ...) standardGeneric('read_vplot'))
 
@@ -22,13 +22,13 @@ setGeneric('fit', function(model, x, ...) standardGeneric('fit'))
 
 setGeneric('encode', function(model, x, ...) standardGeneric('encode'))
 
-setGeneric('as_vplot_autoencoder_cluster_v2_model', function(x, ...) standardGeneric('as_vplot_autoencoder_cluster_v2_model'))
+#setGeneric('as_vplot_autoencoder_cluster_v2_model', function(x, ...) standardGeneric('as_vplot_autoencoder_cluster_v2_model'))
 
-setGeneric('save_model', function(x, dir, ...) standardGeneric('save_model'))
+#setGeneric('save_model', function(x, dir, ...) standardGeneric('save_model'))
 
-setGeneric('load_model', function(dir, ...) standardGeneric('load_model'))
+#setGeneric('load_model', function(dir, ...) standardGeneric('load_model'))
 
-setGeneric('smooth_vplot', function(x, ...) standardGeneric('smooth_vplot'))
+#setGeneric('smooth_vplot', function(x, ...) standardGeneric('smooth_vplot'))
 
 setGeneric('predict', function(model, x, ...) standardGeneric('predict'))
 
@@ -38,7 +38,7 @@ setGeneric('simulate_vplot', function(x, ...) standardGeneric('simulate_vplot'))
 
 setGeneric('prepare_vplot', function(x, ...) standardGeneric('prepare_vplot'))
 
-setGeneric('results', function(x, targets, ...) standardGeneric('results'))
+#setGeneric('results', function(x, targets, ...) standardGeneric('results'))
 
 setGeneric('extract_blocks', function(x, ...) standardGeneric('extract_blocks'))
 
@@ -46,9 +46,9 @@ setGeneric('add_nucleosome_signal', function(x, ...) standardGeneric('add_nucleo
 
 setGeneric('add_track', function(x, ...) standardGeneric('add_track'))
 
-setGeneric('save_results', function(x, dir, ...) standardGeneric('save_results'))
+#setGeneric('save_results', function(x, dir, ...) standardGeneric('save_results'))
 
-setGeneric('load_results', function(dir, ...) standardGeneric('load_results'))
+#setGeneric('load_results', function(dir, ...) standardGeneric('load_results'))
 
 setGeneric('add_kmers', function(x, k, genome, ...) standardGeneric('add_kmers'))
 
