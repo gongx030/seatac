b6e4e960a586cfc208fa1bfa4317690b0659dd70
Wuming Gong <gongx030@umn.edu>
2020-09-04 09:44:26 -0500

    update

--- R/utils.r
+++ R/utils.r
@@ -186,7 +186,10 @@ scale_vplot <- function(x){
 }
 
 
-select_blocks <- function(x, block_size, min_reads, batch_size = 128, with_kmers = FALSE){
+select_blocks <- function(x, block_size, min_reads, batch_size = 128, with_kmers = FALSE, with_nucleoatac = FALSE){
+
+	kmers <- NULL
+	nucleoatac <- NULL
 
 	starts <- seq(1, length(x), by = batch_size)
 	ends <- starts + batch_size - 1
@@ -195,7 +198,7 @@ select_blocks <- function(x, block_size, min_reads, batch_size = 128, with_kmers
 
 	res <- lapply(seq_len(n_batch), function(j){
 		h <- starts[j]:ends[j]
-	  inputs <- x[h] %>% prepare_blocks(block_size, min_reads, with_kmers = with_kmers)
+	  inputs <- x[h] %>% prepare_blocks(block_size, min_reads, with_kmers = with_kmers, with_nucleoatac = with_nucleoatac)
 		flog.info(sprintf('select blocks | n_batch=%5.d/%5.d | selected=%5.d', j, n_batch, inputs$vplots$shape[[1]]))
 		inputs
 	})
@@ -205,8 +208,10 @@ select_blocks <- function(x, block_size, min_reads, batch_size = 128, with_kmers
 
 	if (with_kmers){
 		kmers <- tf$concat(lapply(res, function(xx) xx$kmers), axis = 0L)
-	}else{
-		kmers <- NULL
+	}
+
+	if (with_nucleoatac){
+		nucleoatac <- tf$concat(lapply(res, function(xx) xx$nucleoatac), axis = 0L)
 	}
 
 	list(vplots = vplots,  kmers = kmers)
@@ -215,7 +220,13 @@ select_blocks <- function(x, block_size, min_reads, batch_size = 128, with_kmers
 
 #' prepare_blocks
 #'
-prepare_blocks <- function(x, block_size, min_reads = 0, with_kmers = FALSE){
+prepare_blocks <- function(x, block_size, min_reads = 0, with_kmers = FALSE, with_nucleoatac = FALSE){
+
+	if (with_kmers && is.null(x$kmers))
+		stop('x$kmers must be available if with_kmers=TRUE')
+
+	if (with_nucleoatac && is.null(x$nucleoatac))
+		stop('x$nucleoatac must be available if with_nucleoatac=TRUE')
 
 	n_bins_per_block <- as.integer(block_size / x@bin_size)
 
@@ -236,52 +247,9 @@ prepare_blocks <- function(x, block_size, min_reads = 0, with_kmers = FALSE){
 	y <- y %>% 
 		tf$boolean_mask(include, axis = 0L)
 
-	if (with_kmers){
-		g <- x$kmers %>%
-			tf$cast(tf$int32) %>%
-			tf$expand_dims(-1L) %>%
-			tf$expand_dims(-1L) %>%
-			tf$image$extract_patches(
-				sizes = c(1L, block_size, 1L, 1L),
-				strides = c(1L, x@bin_size, 1L, 1L),
-					rates = c(1L, 1L, 1L, 1L),
-				padding = 'VALID'
-			) %>%
-			tf$squeeze(axis = 2L)
-
-		g <- g %>%
-			tf$reshape(c(g$shape[[1]] * g$shape[[2]], block_size))
-
-		g <- g %>% tf$boolean_mask(include, axis = 0L)
-	}else{
-		g <- NULL
+	if (with_nucleoatac){
+		browser()
 	}
-
-	list(vplots = y, kmers = g)
-
-} # prepare_blocks
-#' prepare_blocks
-#'
-prepare_blocks <- function(x, block_size, min_reads = 0, with_kmers = FALSE){
-
-	n_bins_per_block <- as.integer(block_size / x@bin_size)
-
-	y <- x %>%
-		prepare_vplot() %>%
-		extract_blocks_from_vplot(n_bins_per_block) 
-	
-	y <- y %>%
-		tf$reshape(c(y$shape[[1]] * y$shape[[2]], y$shape[[3]], y$shape[[4]], 1L))
-
-	h <- y %>%
-		tf$math$count_nonzero(c(1L, 3L), dtype = tf$float32) %>% # number of non-zero pixel per bar
-		tf$math$count_nonzero(1L) %>% # number of bar that have non-zero pixels
-		tf$cast(tf$float32)
-
-	include <- h >= min_reads
-
-	y <- y %>% 
-		tf$boolean_mask(include, axis = 0L)
 
 	if (with_kmers){
 		g <- x$kmers %>%
@@ -308,4 +276,26 @@ prepare_blocks <- function(x, block_size, min_reads = 0, with_kmers = FALSE){
 
 } # prepare_blocks
 
+setMethod(
+	'prepare_vplot',
+	signature(
+		x = 'Vplots'
+	),
+	function(
+		x
+	){
 
+		y <- x$counts %>%
+			as.matrix() %>%
+			reticulate::array_reshape(c(    # convert into a C-style array
+				length(x),
+				x@n_intervals,
+				x@n_bins_per_window,
+				1L
+			)) %>%
+			tf$cast(tf$float32)
+
+		y
+
+	}
+)
