ed2cae3376222383b612bd9959681f444e4675ca
Wuming Gong <gongx030@umn.edu>
2020-10-01 12:26:12 -0500

    update

--- R/AllGeneric.r
+++ R/AllGeneric.r
@@ -1,21 +1,5 @@
-#setGeneric('array_reshape', function(x, dims, ...) standardGeneric('array_reshape'))
-
-#setGeneric('get_motif_vplot', function(x, pwms, ...) standardGeneric('get_motif_vplot'))
-
-#setGeneric('as_sparse_array', function(x, ...) standardGeneric('as_sparse_array'))
-
 setGeneric('read_bam', function(filename, peaks, genome, ...) standardGeneric('read_bam'))
-
-setGeneric('get_counts', function(x, filenames, genome, ...) standardGeneric('get_counts'))
-
-#setGeneric('array_permute', function(x, perm, ...) standardGeneric('array_permute'))
-
-#setGeneric('compute_deviations', function(x, ...) standardGeneric('compute_deviations'))
-
 setGeneric('read_vplot', function(x, filenames, genome, ...) standardGeneric('read_vplot'))
-
-setGeneric('add_gc_content', function(x, genome, ...) standardGeneric('add_gc_content'))
-
 setGeneric('build_model', function(name, x, ...) standardGeneric('build_model'))
 
 setGeneric('fit', function(model, x, ...) standardGeneric('fit'))
