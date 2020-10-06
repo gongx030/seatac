555a46adce323b8f9db7a7f30e57ad40734b7f88
Wuming Gong <gongx030@umn.edu>
2020-10-06 16:30:06 -0500

    update

---
+++ R/select_blocks.r
@@ -0,0 +1,146 @@
+#' select_blocks
+#'
+select_blocks <- function(x, batch_size = 128L, min_reads = 0, n_blocks = NA, ...){
+
+	starts <- seq(1, nrow(x), by = batch_size)
+	ends <- starts + batch_size - 1
+	ends[ends > nrow(x)] <- nrow(x)
+	n_batch <- length(starts)
+
+	if (!is.na(n_blocks))
+		n_blocks_per_batch <- table(factor(sample(1:n_batch, n_blocks, replace = TRUE), 1:n_batch))
+
+	res <- list()
+	for (j in seq_len(n_batch)){
+
+		if (!is.na(n_blocks) && n_blocks_per_batch[j] == 0)
+			next
+
+		h <- starts[j]:ends[j]
+		res[[j]] <- x[h] %>% select_blocks_batch(...)
+
+		# select blocks with minimum reads
+		if (min_reads > 0 && !is.null(res[[j]]$vplots)){
+			include <- res[[j]]$n >= min_reads
+			res[[j]] <- lapply(res[[j]], function(r) tf$boolean_mask(r, include))
+		}
+
+		# randomly sample non-empty blocks
+		if (!is.na(n_blocks)){
+			ns <- res[[j]][[1]]$shape[[1]] # n total blocks in current batch
+			if (n_blocks_per_batch[j] < ns){
+				include <- tf$cast(seq_len(ns) %in% sample(ns, n_blocks_per_batch[j]), tf$bool)
+				res[[j]] <- lapply(res[[j]], function(r) tf$boolean_mask(r, include))
+			}
+		}
+	}
+
+	fields <- names(res[[1]])
+	fields <- fields[!sapply(res[[1]], is.null)]
+	res <- lapply(fields, function(f){
+		tf$concat(lapply(res, function(xx) xx[[f]]), axis = 0L)
+	})
+	names(res) <- fields
+	res
+
+} # 
+
+
+#' select_blocks_batch
+#'
+select_blocks_batch <- function(x, block_size, with_vplots = TRUE, with_kmers = FALSE, with_predicted_counts = FALSE, types = NULL){
+
+	res <- list()
+
+	if (with_kmers && is.null(x@kmers))
+		stop('x$kmers must be available if with_kmers=TRUE')
+
+	n_bins_per_block <- as.integer(block_size / x@bin_size)
+	n_blocks_per_window <- as.integer(x@n_bins_per_window - n_bins_per_block + 1)
+	n_blocks <- as.integer(n_blocks_per_window * nrow(x))
+
+	if (with_vplots){
+
+		y <- assays(x)$counts %>%
+			as.matrix() %>%
+			reticulate::array_reshape(c(    # convert into a C-style array
+				length(x),
+				x@n_intervals,
+				x@n_bins_per_window,
+				1L
+			)) %>%
+			tf$cast(tf$float32) %>%
+				extract_blocks_from_vplot(n_bins_per_block) 
+										
+			y <- y %>%
+				tf$reshape(c(y$shape[[1]] * y$shape[[2]], y$shape[[3]], y$shape[[4]], 1L))
+
+			res$vplots <- y
+
+			res$n <- y %>%
+				tf$math$count_nonzero(c(1L, 3L), dtype = tf$float32) %>% # number of non-zero pixel per bar
+				tf$math$count_nonzero(1L) %>% # number of bar that have non-zero pixels
+				tf$cast(tf$float32)
+
+			w <- y %>% tf$reduce_max(shape(1L), keepdims = TRUE)	# max reads per v-plot
+			y <- y / tf$where(w > 0, w, tf$ones_like(w))	# scale so that the sum of each bar is one
+
+			# add weight for each genomic bin
+			w <- tf$reduce_sum(res$vplots, 1L, keepdims = TRUE) > 0
+			w <- w %>% tf$cast(tf$float32)
+			res$weight <- w
+	}
+
+	if (with_kmers){
+		g <- rowData(x)$kmers %>%
+			tf$cast(tf$int32) %>%
+			tf$expand_dims(-1L) %>%
+			tf$expand_dims(-1L) %>%
+			tf$image$extract_patches(
+				sizes = c(1L, block_size, 1L, 1L),
+				strides = c(1L, x@bin_size, 1L, 1L),
+				rates = c(1L, 1L, 1L, 1L),
+				padding = 'VALID'
+			) %>%
+			tf$squeeze(axis = 2L)
+
+		g <- g %>%
+			tf$reshape(c(g$shape[[1]] * g$shape[[2]], block_size))
+
+		res$kmers <- g
+	}
+
+	if (!is.null(types)){
+		for (type in types){
+			if (is.null(rowData(x)[[type]])){
+				flog.warn(sprintf('field x$%s does not exist', type))
+			}else{
+				if (!is.matrix(rowData(x)[[type]])){
+					flog.warn(sprintf('x$%s is not in a matrix format', type))
+				}else{
+					if (x@n_bins_per_window != ncol(rowData(x)[[type]])){
+						flog.warn(sprintf('ncol(x$%s) must be equal to x@n_bins_per_window', type))
+					}else{
+						v <- rowData(x)[[type]] %>%
+							tf$cast(tf$float32) %>%
+							tf$expand_dims(-1L) %>%
+							tf$expand_dims(-1L) %>%
+							tf$image$extract_patches(
+								sizes = c(1L, n_bins_per_block, 1L, 1L),
+								strides = c(1L, 1L, 1L, 1L),
+								rates = c(1L, 1L, 1L, 1L),
+								padding = 'VALID'
+							) %>%
+							tf$squeeze(axis = 2L)
+
+							v <- v %>%
+								tf$reshape(c(v$shape[[1]] * v$shape[[2]], n_bins_per_block))
+							v <- scale01(v)
+							res[[type]] <- v
+					}
+				}
+			}
+		}
+	}
+	res
+} # prepare_blocks
---
+++ R/utils.r
@@ -0,0 +1,139 @@
+#' extract_blocks_from_vplot
+#'
+extract_blocks_from_vplot <- function(x, n_bins_per_block){
+
+	n_intervals <- x$shape[[2]]
+
+	y <- x %>%
+		tf$image$extract_patches(
+			sizes = c(1L, n_intervals, n_bins_per_block, 1L),
+			strides = c(1L, 1L, 1L, 1L),
+			rates = c(1L, 1L, 1L, 1L),
+			padding = 'VALID'
+		) %>%
+		tf$squeeze(axis = 1L)
+
+	y <- y %>%
+		tf$reshape(c(y$shape[[1]], y$shape[[2]], n_intervals, n_bins_per_block)) %>%
+	tf$expand_dims(-1L)
+
+	y
+}
+
+#' reconstruct_vplot_from_blocks
+#'
+#' https://stackoverflow.com/questions/50706431/please-how-to-do-this-basic-thing-with-tensorflow
+#'
+reconstruct_vplot_from_blocks <- function(x){
+
+	batch <- x$shape[[1]]
+	n_blocks_per_window <- x$shape[[2]]
+	n_intervals <- x$shape[[3]]
+	n_bins_per_block <- x$shape[[4]]
+	window_size <- as.integer(n_blocks_per_window + n_bins_per_block - 1)
+
+	padding <- matrix(as.integer(c(0, 0, 0, 0, 0, n_blocks_per_window * n_intervals)), 3, 2, byrow = TRUE)
+	zeros <- tf$zeros(c(batch, n_blocks_per_window, n_intervals))
+
+	w <- rep(n_bins_per_block, window_size)
+	w[1:n_bins_per_block] <- w[1:n_bins_per_block] - n_bins_per_block:1 + 1
+	w[(window_size - n_bins_per_block + 1):window_size] <- w[(window_size - n_bins_per_block + 1):window_size] - 1:n_bins_per_block + 1
+
+	w <- tf$constant(w, dtype = tf$float32) %>%
+		tf$reshape(c(1L, 1L, window_size, 1L))
+
+	y <- x %>% 
+		tf$transpose(perm = c(0L, 1L, 3L, 2L, 4L)) %>%
+		tf$reshape(c(batch, n_blocks_per_window, n_bins_per_block * n_intervals)) %>%
+		tf$pad(padding, 'CONSTANT')
+
+	y <- tf$concat(list(y, zeros), axis = 2L)
+	y <- y %>% tf$reshape(c(batch, -1L))
+	y <- y[, 1:(y$shape[[2]] - n_blocks_per_window * n_intervals)]
+	y <- y %>% tf$reshape(c(batch, n_blocks_per_window, window_size + 1L, n_intervals))
+	y <- y %>% tf$transpose(perm = c(0L, 1L, 3L, 2L))
+
+	y <- y[, , , 1:window_size]
+
+	y <- y %>% 
+		tf$reduce_sum(axis = 1L) %>%
+		tf$expand_dims(-1L)  %>%
+		tf$multiply(1 / w)
+
+	y
+
+}
+
+#' scale01
+#'
+scale01 <- function(x){
+	x_min <- tf$reduce_min(x, 1L, keepdims = TRUE)
+	x_max <- tf$reduce_max(x, 1L, keepdims = TRUE)
+	w <- x_max - x_min
+	w <- tf$where(w > 0, w, tf$ones_like(w))	# scale so that the sum of each bar is one
+	(x - x_min) / w
+}
+
+#' get_angles
+#' Adopted from https://www.tensorflow.org/tutorials/text/transformer
+#'
+get_angles <- function(pos, i, d_model){
+	angle_rates <- 1 / (10000^ ( (2 * i %/% 2) / d_model ))
+  pos %*% angle_rates
+}
+
+positional_encoding <- function(position, d_model){
+	angle_rads <- get_angles(
+		matrix(0:(position - 1), position, 1),
+		matrix(0:(d_model - 1), 1, d_model),
+		d_model
+	)
+	even <- seq(1, ncol(angle_rads), by = 2)
+	angle_rads[, even] <- sin(angle_rads[, even])
+	odd <- seq(2, ncol(angle_rads), by = 2)
+	angle_rads[, odd] <- cos(angle_rads[, odd])
+	angle_rads
+} # positional_encoding
+
+
+#' split_dataset
+#'
+split_dataset <- function(x, test_size = 0.15, batch_size = 64L){
+
+	n <- as.numeric(x$cardinality()) # total number of samples
+	n_train <- as.integer((1 - test_size) * n)  # train samples
+
+	train <- x %>%
+		dataset_take(n_train) %>%
+		dataset_batch(batch_size)
+
+	test <- x %>%
+		dataset_skip(n_train) %>%
+		dataset_batch(batch_size)
+
+	list(train = train, test = test)
+
+} # split_dataset
+
+
+#' load_pretrained_vae_model
+#'
+#' Load pretrained sequence agnostic VAE model for V-plot
+#'
+#' @export
+#' @author Wuming Gong (gongx030@umn.edu)
+#'
+load_pretrained_vplot_vae_model <- function(n_intervals = 48L, block_size = 240L){
+
+	model_file <- system.file('extdata', 'model.h5', package = 'seatac')
+
+	flog.info(sprintf('load_pretrained_vplot_vae_model | model file=%s', model_file))
+	model <- VaeModel(block_size = block_size, n_intervals = n_intervals)
+	res <- model(tf$random$uniform(shape(1L, n_intervals, model$n_bins_per_block, 1L)))
+	model$load_weights(model_file)
+
+	flog.info(sprintf('load_pretrained_vplot_vae_model | width=%d | height=%d | bin size=%d', model$decoder$vplot_decoder$vplot_width, model$decoder$vplot_decoder$vplot_height, model$bin_size))
+
+	model
+
+} # load_pretrained_vplot_vae_model
