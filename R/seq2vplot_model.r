d5c53e0e877d3a15bf2cca59dd6ad3de98aaf212
Wuming Gong <gongx030@umn.edu>
2020-10-12 23:30:37 -0500

    add CustomSchedule

--- R/seq2vplot_model.r
+++ R/seq2vplot_model.r
@@ -1,8 +1,32 @@
+#' CustomSchedule
+#' 
+#' Use the Adam optimizer with a custom learning rate scheduler according to the formula in paper 'Attention is all you need'
+#' modified from https://www.tensorflow.org/tutorials/text/transformer#optimizer
+#' 
+#' @author Wuming Gong (gongx030@umn.edu)
+#'
+CustomSchedule <- reticulate::PyClass(
+	'CustomSchedule',
+	inherit = tf$keras$optimizers$schedules$LearningRateSchedule,
+	list(
+		`__init__` = function(self, d_model, warmup_steps = 4000L){
+			self$d_model <- d_model
+			self$d_model <- tf$cast(self$d_model, tf$float32)
+			self$warmup_steps <- warmup_steps
+			NULL
+		},
+		`__call__` = function(self, step){
+			arg1 <- tf$math$rsqrt(step)
+			arg2 <- step * (self$warmup_steps^(-1.5))
+			tf$math$rsqrt(self$d_model) * tf$math$minimum(arg1, arg2)
+		}
+	)
+)
+
 #' Seq2VplotModel
 #' 
-#' @param
-#'
 #' @export
+#'
 #' @author Wuming Gong (gongx030@umn.edu)
 #'
 setMethod(
@@ -127,7 +151,7 @@ setMethod(
 	){
 
 		# Adopted from https://www.tensorflow.org/tutorials/text/transformer#optimizer
-		learning_rate <- 1e-4
+		learning_rate <- CustomSchedule(model@model$d_model)
 		optimizer <- tf$keras$optimizers$Adam(learning_rate, beta_1 = 0.9, beta_2 = 0.98, epsilon = 1e-9)
 		train_loss <- tf$keras$losses$MeanSquaredError(reduction = 'none')
 
