724b6c0cd440327f9b9cd7374490e9ffce0128fe
Wuming Gong <gongx030@umn.edu>
2020-10-21 14:33:33 -0500

    update

--- R/utils.r
+++ R/utils.r
@@ -130,17 +130,31 @@ load_pretrained_vplot_vae_model <- function(block_size = 240L, latent_dim = 10L,
 
 	message(sprintf('load_pretrained_vplot_vae_model | a pretrained model on GM12878 | block_size=%d', block_size))
 
-	dir <- system.file('extdata', package = 'seatac')
-	model_file <- sprintf('%s/model=Vae_block_size=%d_latent_dim=%d_filters0=%d_min_reads=%d', dir, block_size, latent_dim, filters0, min_reads)
+	model_id <- sprintf('https://s3.msi.umn.edu/gongx030/projects/seatac/models/model=Vae_block_size=%d_latent_dim=%d_filters0=%d_min_reads=%d', block_size, latent_dim, filters0, min_reads)
+	model_index_file <- sprintf('%s.index', model_id)
+	model_data_file <- sprintf('%s.data-00000-of-00001', model_id)
 
-	model_index_file <- sprintf('%s.index', model_file)
-	if (!file.exists(model_index_file))
-		stop(sprintf('%s does not exist', model_index_file))
+	local_model_id <- tempfile()
+	local_model_index_file <- sprintf('%s.index', local_model_id)
+	local_model_data_file <- sprintf('%s.data-00000-of-00001', local_model_id)
+
+	tryCatch({
+		download.file(model_index_file, local_model_index_file)
+	}, error = function(e){
+		stop(sprintf('failed downloading %s', model_index_file))
+	})
+
+	tryCatch({
+		download.file(model_data_file, local_model_data_file)
+	}, error = function(e){
+		stop(sprintf('failed downloading %s', model_data_file))
+	})
 
 	model <- VaeModel(block_size = block_size, n_intervals = n_intervals)
 	res <- model(tf$random$uniform(shape(1L, n_intervals, model$n_bins_per_block, 1L)))
-	load_model_weights_tf(model, model_file)
+	load_model_weights_tf(model, local_model_id)
 	new('VaeModel', model = model)
+
 } # load_pretrained_vplot_vae_model
 
 
