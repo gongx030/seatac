ce4c484956b481f04ddbd54b047dfdda8655ec6a
Wuming Gong <gongx030@umn.edu>
2020-10-22 17:00:23 -0500

    init

---
+++ R/count_reads.r
@@ -0,0 +1,46 @@
+#' Count reads
+#'
+#' Count how many reads fall into a specific fragment size range
+#' 
+#' @export
+#' @author Wuming Gong (gongx030@umn.edu)
+#'
+setMethod(
+	'count_reads',
+	signature(
+		x = 'GRanges',
+		filename = 'character',
+		genome = 'BSgenome'
+	), 
+	function(
+		x, 
+		filename, 
+		genome,
+		fragment_size_range = c(50, 690)
+	){
+
+		window_size <- width(x)
+
+		if (length(unique(window_size)) > 1)
+			stop('the window size of input data must be equal')
+
+		g <- read_bam(filename, peaks = resize(x, fix = 'center', width = window_size + 2000), genome = genome)
+
+		seqlevels(gr, pruning.mode = 'coarse') <- seqlevels(g)
+	  seqlengths(seqinfo(gr)) <-  seqlengths(seqinfo(g))
+  	genome(seqinfo(gr)) <-  genome(seqinfo(g))
+		
+		g <- g[strand(g) == '+']
+		g <- GRanges(
+			seqnames = seqnames(g), 
+			range = IRanges(start(g) + round(mcols(g)$isize / 2), width = 1), 
+			isize = mcols(g)$isize,
+			seqinfo = seqinfo(g),
+			seqlengths = seqlengths(g)
+		)
+		g <- g[g$isize >= fragment_size_range[1] & g$isize <= fragment_size_range[2]]
+
+		coverage(g)[gr] %>% sum()
+	}
+)
+	
