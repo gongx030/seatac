7d842408504b1c7ffef51bc9cb1a33a6a9beae34
Wuming Gong <gongx030@umn.edu>
2021-10-10 11:46:24 -0500

    update

--- R/seatac.r
+++ R/seatac.r
@@ -11,8 +11,8 @@
 #' @importFrom keras keras_model_custom save_model_weights_tf load_model_weights_tf
 #' @importFrom tensorflow shape tf_function
 #' @importFrom SummarizedExperiment SummarizedExperiment rowRanges rowData colData assays rbind rowData<- assays<-
-#' @importFrom GenomicRanges resize reduce granges GRangesList slidingWindows width GRanges seqnames start coverage findOverlaps strand
-#' @importFrom GenomeInfoDb seqlengths seqlevels seqinfo seqlevels<- seqlengths<- seqinfo<- genome genome<-
+#' @import GenomicRanges 
+#' @import GenomeInfoDb 
 #' @importFrom Rsamtools testPairedEndBam ScanBamParam scanBamFlag idxstatsBam indexBam
 #' @importFrom GenomicAlignments readGAlignmentPairs readGAlignments GAlignmentsList
 #' @importFrom gplots colorpanel
