63310dfed79382d2c946c596dec6798cdd5bf660
Wuming Gong <gongx030@umn.edu>
2022-03-01 22:01:26 -0600

    change plotting engine to ggplot2

--- R/vplot.r
+++ R/vplot.r
@@ -4,7 +4,7 @@
 #' 
 #' @param x a Vplots object
 #' @param field  The assays field that is used for Vplots (default: 'counts')
-#' @param ... Arguments passed to vplot_core()
+#' @param ncol number of columns (default: 2L)
 #'
 #' @export
 #'
@@ -16,9 +16,9 @@ setMethod(
 	function(
 		x,
 		field = 'counts',
-		...
+		ncol = 2L
 	){
-		vplot_core(x, field, ...)
+		vplot_core(x, field, ncol)
 	}
 )
 
@@ -29,20 +29,43 @@ setMethod(
 #' 
 #' @param x a Vplots object
 #' @param field  The assays field that is used for Vplots (default: 'counts')
-#' @param ... Arguments passed to image()
+#' @param ncol number of columns (default: 2L)
+#' @import ggplot2
 #'
-vplot_core <- function(x, field, ...){
+vplot_core <- function(x, field, ncol = 2){
+
+	stopifnot(!is.null(assays(x)[[field]]))
 
 	z <- assays(x)[[field]]
-	w <- 1 / rowSums(z)
-	w[is.infinite(w)] <- 0
-	z <- Diagonal(x = w) %*% z
-	z <- colSums(z)
-	z <- z / sum(z)
-	z <- matrix(z, x@n_bins_per_window, x@n_intervals) 
 
-	image(x = x@positions, y = x@centers, z, col = colorpanel(100, low = 'blue', mid = 'white', high = 'red'), xaxt = 'n', yaxt = 'n', xlab = '', ylab = 'fragment size', ...)
-	abline(v = 0, lty = 2, col = 'yellow')
-	axis(1, c(min(x@positions), 0, max(x@positions)), c(-x@window_size / 2, 0, x@window_size / 2))
-	axis(2, seq(0, x@fragment_size_range[2], by = 100L))
+	df <- do.call('rbind', lapply(1:x@n_samples, function(i){
+		zi <- z[, colData(x)$batch == x@samples[i], drop = FALSE]
+		w <- 1 / rowSums(zi)
+		w[is.infinite(w)] <- 0
+		zi <- Diagonal(x = w) %*% zi
+		zi <- colSums(zi)
+		zi <- zi / sum(zi)
+		data.frame(value = zi, bin = x@positions[colData(x)$bin], interval = x@centers[colData(x)$interval], batch = x@samples[i])
+	}))	%>%
+		mutate(batch = factor(batch, x@samples))
+
+	df %>%
+		ggplot(aes(x = bin, y = interval, fill = value)) +
+			geom_raster() +
+			facet_wrap( ~ batch, ncol = ncol) +
+			theme(panel.spacing = unit(2, 'lines')) +
+			geom_vline(xintercept = 0, linetype = 'dotted', color = 'yellow') +
+			scale_fill_gradientn(colors = colorpanel(100, low = 'blue', mid = 'white', high = 'red')) +
+			scale_x_continuous(
+				breaks = c(-x@window_size / 2, 0, x@window_size / 2),
+				limits = c(-x@window_size / 2, x@window_size / 2),
+				expand = c(0, 0)
+			) + 
+			scale_y_continuous(
+				breaks = seq(0,  x@fragment_size_range[2], by = 100L),
+				limits = c(0, max(x@centers)),  
+				expand = c(0, 0)
+			) + 
+			xlab('') + ylab('fragment size')
+
 } # vplot_core
