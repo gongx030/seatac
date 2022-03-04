f51fd4666e439d97336035d1ab421f120c424ae2
Wuming Gong <gongx030@umn.edu>
2022-03-04 16:22:15 -0600

    update Vplots def.

--- R/show.r
+++ R/show.r
@@ -12,7 +12,16 @@ setMethod(
 		object = 'Vplots'
 	),
 	function(object){
-		callNextMethod()
+
+#		callNextMethod()
+
+		sprintf('class: %s', class(object)) %>% message()
+		sprintf('dim: %d %d %d %d', nrow(object), x@n_samples, x@n_intervals, x@n_bins_per_window) %>% message()
+		sprintf('assays(%d): %s', length(assays(object, withDimnames = FALSE)), paste0(names(assays(object, withDimnames = FALSE)), collapse = ' ')) %>% message()
+		sprintf('dimdata:') %>% message()
+		for (i in 1:length(object@dimdata)){
+			sprintf('%s: names(%d): %s', names(object@dimdata)[i], ncol(object@dimdata[[i]]), paste(colnames(object@dimdata[[i]]), collapse = ' ')) %>% message()
+		}
 
 		cat(sprintf('## fragment_size_range:%d,%d\n', object@fragment_size_range[1], object@fragment_size_range[2]))
 		cat(sprintf('## fragment_size_interval:%d\n', object@fragment_size_interval))
@@ -20,8 +29,6 @@ setMethod(
 		cat(sprintf('## bin_size:%d\n', object@bin_size))
 		cat(sprintf('## window_size:%d\n', object@window_size))
 		cat(sprintf('## n_bins_per_window:%d\n', object@n_bins_per_window))
-		cat(sprintf('## n_samples:%d\n', object@n_samples))
-		cat(sprintf('## samples:%s\n', paste(object@samples, collapse = ',')))
 	}
 )
 
