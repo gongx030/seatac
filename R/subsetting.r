ee23402bc647be55aed700ac86f283670af36cee
Wuming Gong <gongx030@umn.edu>
2022-03-08 17:59:20 -0600

    init

---
+++ R/subsetting.r
@@ -0,0 +1,29 @@
+setMethod(
+	'[', 
+	'Vplots',
+	function(x, i, j, ..., drop = FALSE){
+
+		if (missing(i) && missing(j))
+			return(x)
+
+		if (!missing(j)){
+			stop('subsetting by non-first dimension of a Vplots object is not implemented yet')
+		}
+
+		if (!missing(i)){
+			x@dimdata[['grange']] <- x@dimdata[['grange']][i, , drop = FALSE]
+		}
+
+		callNextMethod(x, i, j, ..., drop = drop)
+
+	}
+)
+
+setReplaceMethod(
+	'[',
+	'Vplots',
+	function(x, i, j, ..., value){
+		stop('not implemented yet')
+	}
+)
+	
